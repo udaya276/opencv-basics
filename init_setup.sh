9142f8fa1d631c946c3b5f176974c87111c6f32c
udaya276 <60826416+udaya276@users.noreply.github.com>
2022-06-10 21:03:21 +0530

    initial setup

--- init_setup.sh
+++ init_setup.sh
@@ -6,10 +6,11 @@ echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
+echo [$(date)]: "END"
 # echo [$(date)]: "export conda environment"
 # conda env export > conda.yaml
-echo [$(date)]: "create an src/utils directory and files"
-mkdir -p src/utils && touch src/__init__.py src/main.py
+#echo [$(date)]: "create an src/utils directory and files"
+#mkdir -p src/utils && touch src/__init__.py src/main.py
 
 # to remove everything -
 # rm -rf env/ .gitignore conda.yaml README.md .git/
